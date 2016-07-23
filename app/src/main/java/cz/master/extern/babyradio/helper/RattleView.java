bfd8e88ea54610183f167515c3b96b191f536c93
Yasir Iqbal <yasiriqbal776@gmail.com>
2016-07-23 20:49:07 +0500

    First push

--- app/src/main/java/cz/master/extern/babyradio/helper/RattleView.java
+++ app/src/main/java/cz/master/extern/babyradio/helper/RattleView.java
@@ -10,34 +10,39 @@ import android.graphics.RectF;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class RattleView extends View implements SensorEventListener {
+
     // Bounds of screen
     private int xMin = 0;
     private int yMin = 0;
     private int xMax;
     private int yMax;
-    // Ball attributes
+    // Ball attributes 1
     private float ballRadius = 30;
-    private float ballX = 50;
-    private float ballY = 50;
-    private float ballSpeedX = 5;
-    private float ballSpeedY = 3;
+    private float ballX1 = 50;
+    private float ballY1 = 50;
+    private float ballSpeedX1 = 5;
+    private float ballSpeedY1 = 3;
     private float maxSpeed = 20;
-    private RectF ballBounds;
-    private Paint ballColor;
+    private RectF ballBounds1;
+    private Paint ballColor1;
+    // Ball Attribute 2
+    private float ballX2 = 250;
+    private float ballY2 = 50;
+    private float ballSpeedX2 = 5;
+    private float ballSpeedY2 = 3;
+    private RectF ballBounds2;
+    private Paint ballColor2;
     // For touch input
     private float previousX;
     private float previousY;
     private float scalor = 5.0f;
     // For game elements
-    private Paint textColor;
-    private int scoreX;
-    private int scoreY;
-    private int score = 0;
-    private int maxScore = 30;
+
     // Enemy Ball
     private float enemyRadius = 30;
     private float enemyX = 200;
@@ -46,16 +51,22 @@ public class RattleView extends View implements SensorEventListener {
     private float enemySpeedY = 5;
     private RectF enemyBallBounds;
     private Paint enemyColor;
+    Context context;
 
     public RattleView(Context context) {
         super(context);
         // Initialize game elements
-        ballBounds = new RectF();
+        this.context = context;
         enemyBallBounds = new RectF();
-        ballColor = new Paint();
+
+        ballBounds1 = new RectF();
+        ballColor1 = new Paint();
         enemyColor = new Paint();
-        textColor = new Paint();
+
+        ballBounds2 = new RectF();
+        ballColor2 = new Paint();
         this.setFocusableInTouchMode(true);
+
     }
 
     // When view is first created (or changed) set ball's X & Y max and position.
@@ -63,28 +74,26 @@ public class RattleView extends View implements SensorEventListener {
     public void onSizeChanged(int w, int h, int oldW, int oldH) {
         xMax = w - 1;
         yMax = h - 1;
-        scoreX = w / 3;
-        scoreY = h / 2;
+
     }
 
     public void onDraw(Canvas canvas) {
-        // Draw game text elements
-        textColor.setColor(Color.BLACK);
-        textColor.setAlpha(60);
-        textColor.setTextSize(50);
-        String score_str = "Score: " + Integer.toString(score) + "/" + maxScore;
-        canvas.drawText(score_str, scoreX, scoreY, textColor);
         // Draw ball
-        ballBounds.set(ballX - ballRadius, ballY - ballRadius, ballX + ballRadius, ballY + ballRadius);
-        ballColor.setColor(Color.GREEN);
-        canvas.drawOval(ballBounds, ballColor);
+        ballBounds1.set(ballX1 - ballRadius, ballY1 - ballRadius, ballX1 + ballRadius, ballY1 + ballRadius);
+        ballColor1.setColor(Color.GREEN);
+        canvas.drawOval(ballBounds1, ballColor1);
+
+        ballBounds2.set(ballX2 - ballRadius, ballY2 - ballRadius, ballX2 + ballRadius, ballY2 + ballRadius);
+        ballColor2.setColor(Color.RED);
+        canvas.drawOval(ballBounds2, ballColor2);
         // Draw enemy
-        enemyBallBounds.set(enemyX - enemyRadius, enemyY - enemyRadius, enemyX + enemyRadius, enemyY + enemyRadius);
-        enemyColor.setColor(Color.RED);
-        canvas.drawOval(enemyBallBounds, enemyColor);
+//        enemyBallBounds.set(enemyX - enemyRadius, enemyY - enemyRadius, enemyX + enemyRadius, enemyY + enemyRadius);
+//        enemyColor.setColor(Color.RED);
+//        canvas.drawOval(enemyBallBounds, enemyColor);
         // Perform position calculations
-        updateBall();
-        updateEnemy();
+        updateBall1();
+        updateBall2();
+        //updateEnemy();
         // Delay for the old human eyes to catch up
         try {
             Thread.sleep(3);
@@ -93,51 +102,72 @@ public class RattleView extends View implements SensorEventListener {
         invalidate();
     }
 
-    public void updateBall() {
+    public void updateBall1() {
         // ball x & y change based on ball speed
-        ballX += ballSpeedX;
-        ballY += ballSpeedY;
+        ballX1 += ballSpeedX1;
+        ballY1 += ballSpeedY1;
 
         // Detect Wall Collision on horizontal plane
-        if (ballX + ballRadius > xMax) {
-            ballSpeedX = -ballSpeedX;
-            ballX = xMax - ballRadius;
-            enemyWallCollision();
-        } else if (ballX - ballRadius < xMin) {
-            ballSpeedX = -ballSpeedX;
-            ballX = xMin + ballRadius;
-            enemyWallCollision();
+        if (ballX1 + ballRadius > xMax) {
+            ballSpeedX1 = -ballSpeedX1;
+            ballX1 = xMax - ballRadius;
+            // enemyWallCollision();
+        } else if (ballX1 - ballRadius < xMin) {
+            ballSpeedX1 = -ballSpeedX1;
+            ballX1 = xMin + ballRadius;
+            //  enemyWallCollision();
         }
         // Detect Wall Collision on vertical plane
-        if (ballY + ballRadius > yMax) {
-            ballSpeedY = -ballSpeedY;
-            ballY = yMax - ballRadius;
-            enemyWallCollision();
-        } else if (ballY - ballRadius < yMin) {
-            ballSpeedY = -ballSpeedY;
-            ballY = yMin + ballRadius;
-            enemyWallCollision();
+        if (ballY1 + ballRadius > yMax) {
+            ballSpeedY1 = -ballSpeedY1;
+            ballY1 = yMax - ballRadius;
+            //enemyWallCollision();
+        } else if (ballY1 - ballRadius < yMin) {
+            ballSpeedY1 = -ballSpeedY1;
+            ballY1 = yMin + ballRadius;
+            // enemyWallCollision();
         }
+    }
+
+    //******
+
+    public void updateBall2() {
+        // ball x & y change based on ball speed
+        ballX2 += ballSpeedX2;
+        ballY2 += ballSpeedY2;
+
+        // Detect Wall Collision on horizontal plane
+        if (ballX2 + ballRadius > xMax) {
+            ballSpeedX2 = -ballSpeedX2;
+            ballX2 = xMax - ballRadius;
+            // enemyWallCollision();
+        } else if (ballX2 - ballRadius < xMin) {
+            ballSpeedX2 = -ballSpeedX2;
+            ballX2 = xMin + ballRadius;
+            //  enemyWallCollision();
+        }
+        // Detect Wall Collision on vertical plane
+        if (ballY2 + ballRadius > yMax) {
+            ballSpeedY2 = -ballSpeedY2;
+            ballY2 = yMax - ballRadius;
+            //enemyWallCollision();
+        } else if (ballY2 - ballRadius < yMin) {
+            ballSpeedY2 = -ballSpeedY2;
+            ballY2 = yMin + ballRadius;
+            // enemyWallCollision();
+        }
+        float diffX = ballX1 - ballX2;
+        float diffY = ballY1 - ballY2;
+        // Square root each difference squared
+        float diff = (float) Math.sqrt((diffX * diffX) + (diffY * diffY));
+        if (diff <= (ballRadius + ballRadius)) {
+
+        }//end of if
     }
 
 
     public void updateEnemy() {
-        if (score == maxScore + 1) {
-            pauseBalls();
-            // Alert Dialog
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(getContext());
-            alertDialog.setTitle("Level 1 Complete!");
-            alertDialog.setMessage("Congratulations! Now lets see how you like the blue balls...").setCancelable(false);
-            alertDialog.setPositiveButton("Level 2", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-//					Context context = getContext();
-//					Intent intent = new Intent(context, Level2.class);
-//					context.startActivity(intent);
-                }
-            });
-            alertDialog.show();
-        }
+
         // Update position
         enemyX += enemySpeedX;
         enemyY += enemySpeedY;
@@ -162,8 +192,8 @@ public class RattleView extends View implements SensorEventListener {
         }
         // Check for ball collisions: help ==> http://devmag.org.za/2009/04/13/basic-collision-detection-in-2d-part-1/
         // Calculate how far centers of circle are from one another
-        float diffX = ballX - enemyX;
-        float diffY = ballY - enemyY;
+        float diffX = ballX1 - enemyX;
+        float diffY = ballY1 - enemyY;
         // Square root each difference squared
         float diff = (float) Math.sqrt((diffX * diffX) + (diffY * diffY));
         if (diff <= (ballRadius + enemyRadius)) {
@@ -174,23 +204,23 @@ public class RattleView extends View implements SensorEventListener {
     // Touch-input handler
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        float currentX = event.getX();
-        float currentY = event.getY();
-        float deltaX, deltaY;
-        float scalingFactor = scalor / ((xMax > yMax) ? yMax : xMax);
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_MOVE:
-                deltaX = currentX - previousX;
-                deltaY = currentY - previousY;
-                ballSpeedX += deltaX * scalingFactor;
-                ballSpeedY += deltaY * scalingFactor;
-        }
-        // reset to max speed if over
-        radarGun();
-
-        // Save current x, y
-        previousX = currentX;
-        previousY = currentY;
+//        float currentX = event.getX();
+//        float currentY = event.getY();
+//        float deltaX, deltaY;
+//        float scalingFactor = scalor / ((xMax > yMax) ? yMax : xMax);
+//        switch (event.getAction()) {
+//            case MotionEvent.ACTION_MOVE:
+//                deltaX = currentX - previousX;
+//                deltaY = currentY - previousY;
+//                ballSpeedX1 += deltaX * scalingFactor;
+//                ballSpeedY1 += deltaY * scalingFactor;
+//        }
+//        // reset to max speed if over
+//        radarGun();
+//
+//        // Save current x, y
+//        previousX = currentX;
+//        previousY = currentY;
         return true;
     }
 
@@ -213,7 +243,6 @@ public class RattleView extends View implements SensorEventListener {
     }
 
     public void enemyWallCollision() {
-        score += 1;
         scalor += .4;
         float increment = .2f;
         if (enemySpeedX >= 0) {
@@ -229,24 +258,24 @@ public class RattleView extends View implements SensorEventListener {
     }
 
     public void radarGun() {
-        if (ballSpeedX > 0) {
-            ballSpeedX = ((ballSpeedX > maxSpeed) ? maxSpeed : ballSpeedX);
-        } else if (ballSpeedX < 0) {
-            ballSpeedX = ((ballSpeedX < -maxSpeed) ? -maxSpeed : ballSpeedX);
+        if (ballSpeedX1 > 0) {
+            ballSpeedX1 = ((ballSpeedX1 > maxSpeed) ? maxSpeed : ballSpeedX1);
+        } else if (ballSpeedX1 < 0) {
+            ballSpeedX1 = ((ballSpeedX1 < -maxSpeed) ? -maxSpeed : ballSpeedX1);
         }
-        if (ballSpeedY > 0) {
-            ballSpeedY = ((ballSpeedY > maxSpeed) ? maxSpeed : ballSpeedY);
-        } else if (ballSpeedX < 0) {
-            ballSpeedY = ((ballSpeedY < -maxSpeed) ? -maxSpeed : ballSpeedY);
+        if (ballSpeedY1 > 0) {
+            ballSpeedY1 = ((ballSpeedY1 > maxSpeed) ? maxSpeed : ballSpeedY1);
+        } else if (ballSpeedX1 < 0) {
+            ballSpeedY1 = ((ballSpeedY1 < -maxSpeed) ? -maxSpeed : ballSpeedY1);
         }
     }
 
     public void resetBalls() {
         ballRadius = 30;
-        ballX = ballRadius;
-        ballY = ballRadius;
-        ballSpeedX = 3;
-        ballSpeedY = 5;
+        ballX1 = ballRadius;
+        ballY1 = ballRadius;
+        ballSpeedX1 = 3;
+        ballSpeedY1 = 5;
         enemyRadius = ballRadius;
         enemyX = xMax / 3;
         enemyY = yMax / 3;
@@ -256,10 +285,10 @@ public class RattleView extends View implements SensorEventListener {
 
     public void pauseBalls() {
         ballRadius = 0;
-        ballX = ballRadius;
-        ballY = ballRadius;
-        ballSpeedX = 0;
-        ballSpeedY = 0;
+        ballX1 = ballRadius;
+        ballY1 = ballRadius;
+        ballSpeedX1 = 0;
+        ballSpeedY1 = 0;
         enemyRadius = ballRadius;
         enemyX = 500;
         enemyY = 500;
@@ -268,8 +297,11 @@ public class RattleView extends View implements SensorEventListener {
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
-
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float f = sensorEvent.values[0];
+        float f2 = sensorEvent.values[1];
+        float f3 = sensorEvent.values[2];
+        Log.d("F:f2:f3", f + ":" + f2 + ":" + f3);
     }//end of onSensorChange
 
     @Override
